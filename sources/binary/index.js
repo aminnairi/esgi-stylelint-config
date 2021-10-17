f00ce8d16563843a2fb1422cf6991f678b8b66e4
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-10-17 19:14:00 +0200

    Using the string formatter in the CLI output (#18)
    * using the string formatter in the cli output
    
    * now testing only if the binary has been added

--- sources/binary/index.js
+++ sources/binary/index.js
@@ -2,15 +2,14 @@ const stylelint = require("stylelint");
 const stylelintOptions = require("../index.js");
 
 stylelint.lint({
+  formatter: "string",
   config: stylelintOptions,
   files: (process.argv[2] || "").split(/\s+/)
 }).then(result => {
   if (result.errored) {
-    const errors = JSON.parse(result.output);
-    const message = errors.map(error => error.warnings.map(warning => `${error.source}:${warning.line}:${warning.column}: ${warning.text}`).join("\n")).join("\n");
-
-    throw new Error(message);
+    throw new Error(result.output);
   }
 }).catch(error => {
   console.error(error.message);
+  process.exit(1);
 });
