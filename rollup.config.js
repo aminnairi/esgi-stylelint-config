cfece44ad33b9b6b7e166f3281976823f8be2b50
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-10-17 13:46:55 +0200

    Added a CLI (#14)
    * added tests for the cli
    
    * added service for using node in the terminal
    
    * added the binary and stylelint as dependency
    
    * added bundling for the binary
    
    * initial commit
    
    * trying to solve root issue for file creation
    
    * solved diff space differences issue
    
    * trying to diff directly from the stdin
    
    * fixed the diff file
    
    * testing the cli first before installing locally
    
    * using npm instead of npx for testing the cli
    
    * fixed the esgi-stylelint command name
    
    * attempt at using npx for cli testing
    
    * fixed npx prompt by using the hidden environment variable
    
    * debug to see what is the output
    
    * running npx as root in the action
    
    * using npx locally
    
    * removed all occurrences of docker compose
    
    * fixed the missing stdin option for diff
    
    * fixed the expected output
    
    * trying to ignore space diffs
    
    * attempt at using cmp instead of diff
    
    * attempt at working in two steps
    
    * added output debug
    
    * ignoring all npx debug output
    
    * even more debug
    
    * trying to output to a file
    
    * last attempt
    
    * no output diff checking for the cli
    
    * using npx locally
    
    * using the fully qualified package name
    
    * listing binaries
    
    * listing binaries in the e2e folder
    
    * listing binaries in the e2e folder
    
    * attempt at using the cli
    
    * using node to trigger the cli
    
    * fixed the name of the cli
    
    * fixed the path to the node modules
    
    * using the e2e bad css test file
    
    * testing the cli output
    
    * fixed missing operand for diff
    
    * last attempt at diffing
    
    * removing diffing the binary
    
    * removed the now unecessary output file for diffing

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "sources/index.js",
-  plugins: [
-    terser()
-  ],
-  output: {
-    file: "build/index.js",
-    format: "cjs"
+export default [
+  {
+    input: "sources/index.js",
+    plugins: [
+      terser()
+    ],
+    output: {
+      file: "build/index.js",
+      format: "cjs"
+    }
+  },
+  {
+    input: "sources/binary/index.js",
+    plugins: [
+      terser()
+    ],
+    external: ["stylelint"],
+    output: {
+      file: "build/binary/index.js",
+      format: "cjs",
+      banner: "#!/usr/bin/env node"
+    }
   }
-}
+]
